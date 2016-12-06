87942a84449cb37da88399a691b599dafa724dc1
Marcos Mtz <marcosmtz@MacBook-Pro-de-Marcos.local>
2016-12-05 23:05:13 -0600

    Ya esta con el nombre todo ojala y se suba bien

--- app/src/main/java/com/example/marcosmtz/littlepig/LittlePigAdminUser.java
+++ app/src/main/java/com/example/marcosmtz/littlepig/LittlePigAdminUser.java
@@ -9,32 +9,39 @@ import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LittlePigAdminUser extends AppCompatActivity implements View.OnClickListener{
 
     private FirebaseAuth firebaseAuth;
     private TextView textViewUserEmail;
     private Button buttonLogOut;
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_little_pig_admin_user);
-
+        databaseReference = FirebaseDatabase.getInstance().getReference();
         firebaseAuth = FirebaseAuth.getInstance();
 
         if (firebaseAuth.getCurrentUser() == null){
             finish();
             startActivity(new Intent(this, LittlePigLogIn.class));
         }
-
-        FirebaseUser user = firebaseAuth.getCurrentUser();
+        LoadName ();
+        //Obtener Correo
+        /*FirebaseUser user = firebaseAuth.getCurrentUser();
 
         textViewUserEmail = (TextView) findViewById(R.id.textViewUserEmail);
         textViewUserEmail.setText("Bienbenido " + user.getEmail());
-
         buttonLogOut = (Button) findViewById(R.id.buttonLogout);
-
+        buttonLogOut.setOnClickListener(this);*/
+        buttonLogOut = (Button) findViewById(R.id.buttonLogout);
         buttonLogOut.setOnClickListener(this);
     }
 
@@ -42,6 +49,34 @@ public class LittlePigAdminUser extends AppCompatActivity implements View.OnClic
     public void onClick(View view) {
         if (view == buttonLogOut){
             firebaseAuth.signOut();
+            finish();
+            startActivity(new Intent(this, LittlePigLogIn.class));
+        }
+    }
+
+    private void LoadName (){
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        DatabaseReference ref = database.getReference("UserData/" + user.getUid() + "/fullName");
+
+        try {
+            ref.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    String value = dataSnapshot.getValue(String.class);
+
+                    textViewUserEmail = (TextView) findViewById(R.id.textViewUserEmail);
+
+                    textViewUserEmail.setText("Bienbenido " + value);
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    //textViewUserEmail.setText("Bienbenido " + user.getEmail());
+                }
+            });
+        }catch (Exception e){
             finish();
             startActivity(new Intent(this, LittlePigLogIn.class));
         }
